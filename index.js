58cfb0f6ea8f6026e0582891ab0cbd5373280b48
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-04-17 12:35:16 +1200

    Added base files to work with

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+const app = express();
+const port = 7500;
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const data = {
+    name: 'Vishesh Kudva',
+    age: 13,
+    hobbies: ['Coding', 'Gaming', 'Reading'],
+    place: 'New Zealand',
+    job: 'Student',
+    langs: ['JavaScript', 'HTML', 'CSS'],
+};
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { data });
+}).use((eq, res) => {
+    res.status(404).send('ERROR_404_PAGE_NOT_FOUND');
+}).listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+setInterval(() => {
+    fetch("https://v-blogger.onrender.com")
+        .catch(err => {
+            console.error("Ping failed:", err);
+        });
+}, 60000); // 60,000 milliseconds = 1 minute
