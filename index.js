53370a3492e603231d4c81d9a9b9e3b962fcbcdd
Vishesh Kudva <vishesh.kudva@outlook.com>
2026-01-21 15:47:00 +1300

    Smart Search Console tagging

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const app = express();
 const port = process.env.PORT || 7500;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+let render = false;
+
+if (process.env.HOST == "render") {
+  render = true;
+}
 
 app
   .get("/", (req, res) => {
-    res.render("index.ejs", { data, paras });
+    res.render("index.ejs", { data, paras, render });
   })
   .use((eq, res) => {
     res.status(404).send("ERROR_404_PAGE_NOT_FOUND");
